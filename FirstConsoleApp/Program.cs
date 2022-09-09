1b313d349cdaa4208b074754addcf4ea46182a3e
Vedat <Vedat@DESKTOP-F7B40NP>
2022-09-09 17:07:23 +0300

    Add project files.

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+
+namespace FirstConsoleAp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name;
+            string surname;
+            Console.WriteLine("Hello World!");
+            /* Console.WriteLine("Hey programmer");
+              Console.BackgroundColor = ConsoleColor.Yellow;
+              Console.BackgroundColor = ConsoleColor.Red;
+                Console.ReadLine();  */
+
+            Console.Write("Adınızı giriniz:");
+            name = Console.ReadLine();
+            Console.Write("Soyadınızı giriniz:");
+            surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba: "+name+" "+surname);
+        }
+    }
+}
